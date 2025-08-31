87a5fe647cefb36a5db317504822959d0fbe0b50
David Balaguer <david.balaguer@accuro.es>
2025-08-31 16:56:52 +0200

    Add GitHub Actions workflow for deploying to GitHub Pages and configure Astro site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Davcer99.github.io', 
+  base: 'curriculum_natalia'
+});
